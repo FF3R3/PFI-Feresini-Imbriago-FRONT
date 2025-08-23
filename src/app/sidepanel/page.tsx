cd3102d1f11912e561eb287df4a75da78d551c5e
Thomas Imbriago <thomas.imbriago29@gmail.com>
2025-08-23 18:16:48 -0300

    tailwind config

---
+++ src/app/sidepanel/page.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import {
+  meet,
+  MeetSidePanelClient,
+} from "@googleworkspace/meet-addons/meet.addons";
+import { CLOUD_PROJECT_NUMBER, MAIN_STAGE_URL } from "../../constants";
+
+/**
+ * See: https://developers.google.com/meet/add-ons/guides/overview#side-panel
+ */
+export default function Page() {
+  const [sidePanelClient, setSidePanelClient] = useState<MeetSidePanelClient>();
+
+  // Launches the main stage when the main button is clicked.
+  async function startActivity(e: unknown) {
+    if (!sidePanelClient) {
+      throw new Error("Side Panel is not yet initialized!");
+    }
+    console.log("Starting mainstage on:", MAIN_STAGE_URL);
+
+    await sidePanelClient.startActivity({ mainStageUrl: MAIN_STAGE_URL });
+  }
+
+  /**
+   * Prepares the Add-on Side Panel Client.
+   */
+  useEffect(() => {
+    (async () => {
+      const session = await meet.addon.createAddonSession({
+        cloudProjectNumber: CLOUD_PROJECT_NUMBER,
+      });
+      setSidePanelClient(await session.createSidePanelClient());
+    })();
+  }, []);
+
+  return (
+    <>
+      <h1 className="h1 text-center">
+        Comenza tu sesion de estudio en con Sparkle!
+      </h1>
+      <div className="text-center">
+        <button className="btn-primary" onClick={startActivity}>
+          Obtener datos
+        </button>
+      </div>
+    </>
+  );
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <header className="py-4 flex items-center justify-center">
+      <img
+        src="/logos/completo.png"   // reemplaza con la ruta de tu logo
+        alt="Logo de Mi App"
+        className="h-10 w-auto"
+      />
+    </header>
+  )
+}
