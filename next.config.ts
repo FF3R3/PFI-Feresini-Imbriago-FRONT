c984c662cd892256426b3560e612d289b040df02
Thomas Imbriago <thomas.imbriago29@gmail.com>
2025-08-26 19:27:17 -0300

    edit config

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,10 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
-  basePath: isProd ? '/PFI-Feresini-Imbriago-FRONT' : '',
-  assetPrefix: isProd ? '/PFI-Feresini-Imbriago-FRONT/' : '',
+  basePath: '/PFI-Feresini-Imbriago-FRONT',
+  assetPrefix: '/PFI-Feresini-Imbriago-FRONT/',
   images: {
     unoptimized: true
   }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <header className="py-8 flex items-center justify-center">
       <img
-        src="/logos/completo.png"   // reemplaza con la ruta de tu logo
+        src="/logos/completo.png"
         alt="Logo de Mi App"
         className="h-10 w-auto"
       />
